5b1af7e6c992d7140e7ac00a0c542e55d39fa8e7
pradyotpsahoo <42238958+pradyotpsahoo@users.noreply.github.com>
2018-08-09 16:58:46 +0530

    version 1.0

---
+++ sample.cpp
@@ -0,0 +1,9 @@
+#include <iostream> // library					
+using namespace std; // Resolve multiple definition issue of same name functions. 
+
+int main(){
+        cout << "Hello"; // Prints to terminal
+        return 10 ;
+}
+
+
